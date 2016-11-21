e18160eda56d47ff0c333de8509cdb9b001e2054
Madeline Hsia <madeline.hsia422@gmail.com>
2016-11-21 00:15:51 -0800

    NEW SITE

---
+++ js/ani.js
@@ -0,0 +1,31 @@
+var initialLoad = true;
+
+$(document).ready(function () {
+
+console.log(document.referrer.indexOf(window.location.hostname));
+if ( document.referrer == null || document.referrer.indexOf(window.location.hostname) == 0 ) {
+
+  var b = function($b,speed){
+      beeWidth = $b.width();
+      
+      $b.delay(1000).animate({
+          top:"4%"
+      }, speed, function() {
+        $('.hide').css("display", "block");
+    });
+  };
+
+  $(function(){
+      b($("#b"), 1000);
+  });
+
+    $('#container-single article').delay(2000).animate({
+        left: '-490',
+        easing: 'easeOutQuad'
+    }, 500, function() {
+        $('#hello').css("display", "block");
+    });
+  initialLoad=false;
+}
+
+});
